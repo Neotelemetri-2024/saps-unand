c4b634c24225759ce3d418056cbcbe217d4964c0
shevaramadhan <sheva08.ramadhan@gmail.com>
2026-09-23 12:48:13 +0700

    fix: uploads static path to use __dirname

--- src/index.ts
+++ src/index.ts
@@ -70,7 +70,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use('/uploads', (_req, res, next) => {
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
-}, express.static(path.join(process.cwd(), 'uploads')));
+}, express.static(path.join(__dirname, '../uploads')));
 
 // ==================== ROUTES ====================
 // Health Check
